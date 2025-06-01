3d83d9cdd917dcf47aa430daf898a6f018529af9
rasika <rasika.sugathadasa@e-jan.co.jp>
2025-06-01 23:18:32 +0900

    Add new end points

--- apis/src/main/java/com/moneyforward/apis/GithubApiService.kt
+++ apis/src/main/java/com/moneyforward/apis/GithubApiService.kt
@@ -1,8 +1,10 @@
 package com.moneyforward.apis
 
+import com.moneyforward.apis.model.Profile
 import com.moneyforward.apis.model.RepositoryList
 import com.moneyforward.apis.model.SearchUserResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 /**
@@ -20,14 +22,20 @@ interface GithubApiService {
         @Query("q") userName: String
     ): SearchUserResponse
 
+
+    @GET("users/{login}")
+    suspend fun profile(
+        @Path("login") login: String
+    ): Profile
+
     /**
      * Fetches a list of repositories from the Github API.
-     * @param userName The username of the user to fetch repositories for.
+     * @param login The username of the user to fetch repositories for.
      * @return A list of [RepositoryList] objects.
      * Ex: https://api.github.com/users/{userName}/repos
      */
-    @GET("users/{userName}/repos")
-    suspend fun repositories(
-        userName: String
-    ): List<RepositoryList>
+    @GET("users/{login}/repos")
+    suspend fun repos(
+        @Path("login") login: String
+    ): RepositoryList
 }
